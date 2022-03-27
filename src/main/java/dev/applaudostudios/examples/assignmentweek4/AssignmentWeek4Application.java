43f4b98daf4c43d895586275efe283308bda7405
estefaniag08 <estefaniag08@hotmail.com>
2022-03-27 10:44:40 -0500

    Initialize the project

---
+++ src/main/java/dev/applaudostudios/examples/assignmentweek4/AssignmentWeek4Application.java
@@ -0,0 +1,13 @@
+package dev.applaudostudios.examples.assignmentweek4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentWeek4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentWeek4Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/applaudostudios/examples/assignmentweek4/AssignmentWeek4ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.applaudostudios.examples.assignmentweek4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentWeek4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
